e2e2a317df34a81d49e839a92e705d4de3aaab6a
cacop <pauloricardossfilho@gmail.com>
2024-03-19 22:02:57 -0300

    Continuando controller teste cheio de erros]

--- src/test/java/com/fiap/restaurante/ClienteControllerTest.java
+++ src/test/java/com/fiap/restaurante/ClienteControllerTest.java
@@ -7,7 +7,10 @@ import com.fiap.restaurante.controller.ClienteController;
 
 import com.fiap.restaurante.domain.Cliente;
 import com.fiap.restaurante.domain.dto.ClienteDto;
+import com.fiap.restaurante.domain.dto.EnderecoDto;
+import com.fiap.restaurante.dto.ClienteRequest;
 import com.fiap.restaurante.handler.GlobalExceptionHandler;
+import com.fiap.restaurante.repository.ClienteRepository;
 import com.fiap.restaurante.service.ClienteService;
 import com.fiap.restaurante.utils.ClienteHelper;
 import org.junit.jupiter.api.AfterEach;
@@ -20,6 +23,9 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.UUID;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
@@ -42,6 +48,9 @@ public class ClienteControllerTest {
 
     @Mock
     private ClienteService clienteService;
+
+    @Mock
+    private ClienteRepository clienteRepository;
 
     AutoCloseable openMocks;
 
@@ -84,11 +93,73 @@ public class ClienteControllerTest {
         }
     }
 
+    @Nested
+    class BuscarCliente{
+
+        @Test
+        void devePermetirBuscarClientePorId() throws Exception {
+            var id = UUID.fromString("259bdc02-1ab5-11ee-be56-0242ac120002");
+            var cliente = ClienteHelper.gerarRegistro();
+            cliente.setId(id);
+            var clienteDto = clienteToDto(cliente);
+
+            when(clienteService.buscarPorId(any(UUID.class))).thenReturn(clienteDto);
+
+            mockMvc.perform(get("/clientes/{id}", id)
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.id").value(cliente.getId().toString()))
+                    .andExpect(jsonPath("$.nome").value(cliente.getNome()))
+                    .andExpect(jsonPath("$.email").value(cliente.getEmail()))
+                    .andExpect(jsonPath("$.endereco").exists());
+        }
+
+    }
+
     public static String asJsonString(final Object obj) {
         try {
             return new ObjectMapper().writeValueAsString(obj);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+
+    public ClienteDto clienteToDto(Cliente cliente) {
+        return new ClienteDto(
+                cliente.getId(),
+                cliente.getNome(),
+                cliente.getEmail(),
+                cliente.getFone(),
+                new EnderecoDto(
+                        cliente.getEndereco().getLogradouro(),
+                        cliente.getEndereco().getNumero(),
+                        cliente.getEndereco().getComplemento(),
+                        cliente.getEndereco().getBairro(),
+                        cliente.getEndereco().getCidade(),
+                        cliente.getEndereco().getUf(),
+                        cliente.getEndereco().getCep()
+                )
+        );
+
+    }
+
+    public ClienteDto clienteToDto(ClienteRequest cliente) {
+        return new ClienteDto(
+                cliente.getId(),
+                cliente.getNome(),
+                cliente.getEmail(),
+                cliente.getFone(),
+                new EnderecoDto(
+                        cliente.getEndereco().getLogradouro(),
+                        cliente.getEndereco().getNumero(),
+                        cliente.getEndereco().getComplemento(),
+                        cliente.getEndereco().getBairro(),
+                        cliente.getEndereco().getCidade(),
+                        cliente.getEndereco().getUf(),
+                        cliente.getEndereco().getCep()
+                )
+        );
+
     }
 }
